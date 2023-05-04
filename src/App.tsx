f4659dae18c7491f109adb1ee959cf7a4d09c4b0
Jose Torres <vidojam@gmail.com>
2023-05-04 16:11:03 -0400

    dog objects added for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
 import { Hello } from './components/Hello';
-
+import { Form } from './components/Form'
 
 function App() {
+  const[sever, setServerData] = React.useState([])
+
+  React.useEffect(()=> {
+    fetch('http://18.212.62.122:4000')
+    .then(res => res.json())
+    .then(data => console.log())
+    .catch(err => console.error(err))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <Hello  firstName="Jose" lastName="Sanchez"/>
-
+        <Hello  firstName="Jose" lastName="Torres"/>
+        <Form />
+        {/* @ts-ignore */}
+        {serverData && serverData.map(eachDog =><p>{eachDog.name} - {eachDog.age}</p>)}
       </header>
     </div>
   );
